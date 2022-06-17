8d364b2ce421ccbad0250ee6d855fb2c6fef518f
Manal Elsayed <51231131+manalelsayed@users.noreply.github.com>
2022-06-18 00:20:55 +0200

    Time: 131 ms (48.68%), Space: 35.3 MB (23.20%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public IList<IList<int>> Generate(int numRows) {
+       IList<IList<int>> res = new List<IList<int>>();
+        
+         res.Add(new List<int>(){1});
+        for(int i=1;i<numRows;i++){
+             res.Add(new List<int>());
+             int term = (int)Math.Pow(1, i);
+                
+                res[i].Add(term);
+           
+            for(int a=1;a<i;a++){
+                term = term * 1 * (i - a + 1)
+                                / (a * 1);
+ 
+            res[i].Add(term);
+               
+            }
+           res[i].Add(1);
+        }
+        
+    return res;
+    }
+}
